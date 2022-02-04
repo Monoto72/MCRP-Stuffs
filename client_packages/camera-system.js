c4f1ad1e6ca750545bb5a9250676174b141d9158
UP2017778 <sking233952@gmail.com>
2022-02-04 15:54:51 +0000

    Update(Client-Camera-System): reworked most of the camera/server showcases, also started working on a delete camera via delete key

--- client_packages/camera-system.js
+++ client_packages/camera-system.js
@@ -1,21 +1,36 @@
 let instances = [];
 let currentInstance;
 let browser;
+let withinCameraLocation = false;
+let cameraObject = [];
 
-mp.events.add("cameraShowcase", (type) => {
+
+mp.events.add('serverShowcase', (location, size) => {
+    let serverPreview;
+
+    serverPreview = mp.markers.new(1, location, size, {
+        color: [0, 255, 0, 255]
+    });
+
+    setTimeout(() => {
+        serverPreview.destroy();
+    }, 10000)
+});
+
+mp.events.add('cameraShowcase', (type) => {
     const cameraPreview = new mp.Vector3(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z-1);
 
     currentInstance = localCameras("Currently Placing", cameraPreview, type);
 });
 
-mp.events.add("cameraShowcaseAll", (cameras) => {
+mp.events.add('cameraShowcaseAll', (cameras) => {
     cameras.forEach(element => {
         let cameraLocation = new mp.Vector3(element.location.x, element.location.y, element.location.z-1);
-        instances.push(localCameras(element.name, cameraLocation));
+        instances.push(localCameras(element.name, cameraLocation, element.size, element.serverName));
     });
 })
 
-mp.events.add("cameraColDestroy", () => {
+mp.events.add('cameraColDestroy', () => {
     if (currentInstance) {
         currentInstance.marker.destroy();
         currentInstance.label.destroy();
@@ -27,6 +42,11 @@ mp.events.add("cameraColDestroy", () => {
         });
     }
 });
+
+mp.events.add('withinCameraColshape', (camera) => {
+    withinCameraLocation = true;
+    cameraObject = camera;
+})
 
 mp.keys.bind(0x71, true, () => {
     if (!browser) {
@@ -41,14 +61,44 @@ mp.keys.bind(0x72, true, () => {
     }
 });
 
+mp.keys.bind(0x2E, true, () => {
+    if (withinCameraLocation) {
+        mp.events.callRemote("removeCamera", [cameraObject]);
+        mp.gui.chat.push("Yes")
+        withinCameraLocation = !withinCameraLocation;
+        cameraObject = null;
+    }
+});
 
-localCameras = (name, location, type = 4) => {
+
+localCameras = (name, location, type, serverName = "Currently within", serverID = "") => {
+
+    const labelLocation = location.clone()
+    labelLocation.z = labelLocation.z+1.5;
+
+    size = () => {
+        let size = "";
+
+        switch (type) {
+            case 2:
+                size = "Small";
+                break;
+            case 4:
+                size = "Medium";
+                break;
+            case 6:
+                size = "Large";
+                break;
+            default:
+        }
+        return size;
+    }
 
     const camera = {
         "marker": mp.markers.new(1, location, type, {
             color: [255, 255, 255, 255]
         }),
-        "label": mp.labels.new(name, location, {
+        "label": mp.labels.new(`Camera: ${name} \n Server: ${serverName} \n Size: ${size(type)}`, labelLocation, {
             los: true,
             font: 1,
             drawDistance: 8,
